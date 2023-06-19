0409eae44c6fb5febc822169d257fddf80672982
pablonicovillalba2193@gmail.com <pablonicovillalba2193@gmail.com>
2023-06-18 21:54:24 -0300

    Agregamos la seccion detalles para cada profecion

--- ProyectoFinal/app/src/main/java/com/example/proyectofinal/DoctorDetailsActivity.java
+++ ProyectoFinal/app/src/main/java/com/example/proyectofinal/DoctorDetailsActivity.java
@@ -2,13 +2,34 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
 
+    TextView tv;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
+        tv = findViewById(R.id.textViewDDTitle);
+        btn = findViewById(R.id.buttomDDBack);
+
+        Intent it = getIntent();
+        String title = it.getStringExtra("title");
+        tv.setText(title);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 startActivity(new Intent(DoctorDetailsActivity.this, FindDoctorActivity.class));
+                }
+             }
+        );
+
     }
 }
